51bba89a2f1cc7ff8ae964b1720216b3d327bb07
Marija Milenkovic <makicaaa.2000@gmail.com>
2026-06-09 00:45:19 +0200

    Izmene

--- Backend/src/test/java/com/example/SpringRestAppDemo/service/impl/KorisnickiProfilServiceImplTest.java
+++ Backend/src/test/java/com/example/SpringRestAppDemo/service/impl/KorisnickiProfilServiceImplTest.java
@@ -166,4 +166,42 @@ class KorisnickiProfilServiceImplTest {
 
         assertEquals("Stara lozinka nije tačna!", ex.getMessage());
     }
+    
+    @Test
+    void register_shouldFail_whenEmailInvalidDomain() {
+        RegisterRequestDto dto = new RegisterRequestDto();
+        dto.setEmail("test@gmail.com"); // pogrešan domain
+        dto.setLozinka("Password1!");
+        dto.setUlogaID(1L);
+
+        when(korisnickiProfilRepository.findByEmail(anyString()))
+                .thenReturn(Optional.empty());
+
+        when(ulogaRepository.findById(anyLong()))
+                .thenReturn(Optional.of(new Uloga()));
+
+        Exception ex = assertThrows(Exception.class,
+                () -> service.register(dto));
+
+        assertEquals("Email mora biti u formatu @fon.bg.ac.rs", ex.getMessage());
+    }
+    
+    @Test
+    void register_shouldFail_whenPasswordTooShort() {
+        RegisterRequestDto dto = new RegisterRequestDto();
+        dto.setEmail("test@fon.bg.ac.rs");
+        dto.setLozinka("123"); // loš password
+        dto.setUlogaID(1L);
+
+        when(korisnickiProfilRepository.findByEmail(anyString()))
+                .thenReturn(Optional.empty());
+
+        when(ulogaRepository.findById(anyLong()))
+                .thenReturn(Optional.of(new Uloga()));
+
+        Exception ex = assertThrows(Exception.class,
+                () -> service.register(dto));
+
+        assertEquals("Lozinka mora imati najmanje 8 karaktera", ex.getMessage());
+    }
 }
